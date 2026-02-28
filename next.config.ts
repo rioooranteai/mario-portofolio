77f274467b34b98447257e6c49e8c9b0a31950de
rioooranteai <rioooranteai@gmail.com>
2026-02-28 15:49:12 +0800

    fix: next.config for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: "/mario-portofolio",
-  assetPrefix: "/mario-portofolio",
+  basePath: '/mario-portofolio',
+  assetPrefix: '/mario-portofolio/',
   images: {
     unoptimized: true,
-  }
+  },
 };
 
 export default nextConfig;
